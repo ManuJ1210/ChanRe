42c725ee4b33ccece42e7679fe2a5619106a8f66
Manu J <manunaidu.cj10@gmail.com>
2025-06-24 11:57:13 +0530

    Updated sections-bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './index.css'
 import Header from './components/Header'
-
+import ScrollToTop from './components/topPage'
 import Footer from './components/footer'
 import AppRoutes from './Routers/Navigation'
+
+
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <AppRoutes />
       <Footer />
     </>
